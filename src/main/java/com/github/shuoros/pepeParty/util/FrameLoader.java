b1110bde767ebef7f962fe6e6a0ecadb68aa9817
Soroush <s_shemshadi@ymail.com>
2021-10-20 11:47:26 +0330

    Implement FrameLoader util

---
+++ src/main/java/com/github/shuoros/pepeParty/util/FrameLoader.java
@@ -0,0 +1,38 @@
+package com.github.shuoros.pepeParty.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.github.shuoros.pepeParty.model.Frame;
+
+public class FrameLoader {
+
+	public static List<Frame> load(final String path) throws IOException {
+		List<Frame> frames = new ArrayList<>();
+		for (String file : new BufferedReader(//
+				new InputStreamReader(//
+						Thread.currentThread().getContextClassLoader()//
+								.getResourceAsStream(path),
+						StandardCharsets.UTF_8))//
+								.lines()//
+								.collect(Collectors.toList())//
+		) {
+			frames.add(//
+					new Frame(//
+							new BufferedReader(//
+									new InputStreamReader(//
+											Thread.currentThread().getContextClassLoader()//
+													.getResourceAsStream(file),
+											StandardCharsets.UTF_8)).lines()//
+													.collect(Collectors.joining("\n"))//
+					));
+		}
+		return frames;
+	}
+
+}
